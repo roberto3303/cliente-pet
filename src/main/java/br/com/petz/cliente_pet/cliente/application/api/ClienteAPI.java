1e6017d691ec3d219c319fce85b0765ff8f51014
Roberto Pedro <robertopedro100@gmail.com>
2024-10-15 12:50:02 -0300

    Implementa controller e fluxo para service

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/cliente")
-
 public interface ClienteAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
